48c37f1695359e1dd48477db6f27f21464c5cab6
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2017-08-24 16:11:19 +0300

    Add tests for Guava

---
+++ src/test/resources/AnnotateClassMethodsIntention/guava/before.template.after.java
@@ -0,0 +1,13 @@
+import com.google.common.annotations.VisibleForTesting;
+
+class Foo {
+
+    @VisibleForTesting
+    public void foo() {}
+
+    @VisibleForTesting
+    public void bar() {}
+
+    @VisibleForTesting
+    public void baz() {}
+}
---
+++ src/test/resources/AnnotateClassMethodsIntention/guava/before.template.java
@@ -0,0 +1,8 @@
+class F<caret>oo {
+
+    protected void foo() {}
+
+    void bar() {}
+
+    private void baz() {}
+}
